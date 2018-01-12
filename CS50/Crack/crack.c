4298da9f71c0a552417b276a9eb45cfe18bd063d
VytenisN <vyt.navickas@gmail.com>
2018-01-12 13:14:44 +0200

    Cracking two character long passwords

--- CS50/Crack/crack.c
+++ CS50/Crack/crack.c
@@ -11,33 +11,37 @@
 int main (int argc, string argv[])
 {
     //checking if a single hash is entered into command line
-    if (argc !=2) 
+    if (argc !=2)
     {
         printf("Paste a single hash:");
         return 1;
     }
     char test[6] ;
-    
+
     // *Uncomment 2 lines below in order to create new hashes
     //string z = crypt(argv[1], "50");
     //printf ("%s\n", z);
-    for ( int ii = 65, j = 0; ii < 123; ii++)
-    {
-        for ( int i = 65; i < 123; i++)  //all the possible combos in ASCII
+
+    int size = 0;
+   // for ( int iii = 65 ; iii < 123; iii++)
+    //{
+        for ( int ii = 65 ; ii < 123; ii++)
         {
-
-            if (j==1) test[0] = (char) ii;
-            test[0+j] = (char) i;
-            test[1+j] = '\0';
-            if (strcmp(crypt(test, "50"), argv[1])  == 0) //checking if strings are equal
+            for ( int i = 65 ; i < 123; i++)  //all the possible combos in ASCII
             {
-                printf("%s Yessssss\n",test);
-                return 0;
+                /i/f (size==2) test[0] = (char) iii;
+                if (size==1) test[0] = (char) ii;
+                test[0+size] = (char) i;
+                test[1+size] = '\0';
+                if (strcmp(crypt(test, "50"), argv[1])  == 0) //checking if strings are equal
+                {
+                    printf("%s Yessssss\n",test);
+                    return 0;
+                }
+                printf("%s Not found\n",test );
+                if (size < 1 && i == 122) {size ++; i = 64;}
+                //if (j < 2  i == 122 )
             }
-            printf("%s Not found\n",test );
-            if (j < 2 && i==122) { j++; i = 64;}
         }
-
-    }
-
+    //}
 }
